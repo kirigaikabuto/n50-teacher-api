d0755048d3b7ddfea0bb5b720018ea811081cd30
Yerassyl Tleugazy <tleugazy98@gmail.com>
2022-05-01 01:26:18 +0600

    feat:add teachers subjects

--- subjects/commands.go
+++ subjects/commands.go
@@ -1,1 +1,16 @@
 package subjects
+
+type CreateSubjectCommand struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type ListSubjectsCommand struct {
+}
+
+type GetSubjectByIdCommand struct {
+	Id string `json:"id"`
+}
+
+type CreateTeacherSubject struct {
+}
--- subjects/service.go
+++ subjects/service.go
@@ -6,6 +6,7 @@ import (
 )
 
 type SubjectService interface {
+
 }
 
 type subjectService struct {
